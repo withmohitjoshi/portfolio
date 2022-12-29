7c68ebb1f2111d234ac12307a4bce74825f5fcf7
Mohit Joshi <withmohitjoshi@gmail.com>
2022-12-29 21:25:11 +0530

    New project added

--- src/components/Projects/projectsData.ts
+++ src/components/Projects/projectsData.ts
@@ -1,43 +1,22 @@
 import { projectsImages } from "../../assets/projects";
 
 export const projectsData = [
-  // {
-  //   id: 1,
-  //   name: "Weather App",
-  //   liveUrl: "https://withmohitjoshi.github.io/weather-app/",
-  //   codeUrl: "https://github.com/withmohitjoshi/weather-app",
-  //   imageSrc: projectsImages.weatherApp,
-  // },
-  // {
-  //   id: 2,
-  //   name: "The Todo App",
-  //   liveUrl: "https://withmohitjoshi.github.io/the-todo-app/",
-  //   codeUrl: "https://github.com/withmohitjoshi/the-todo-app",
-  //   imageSrc: projectsImages.theTodoApp,
-  // },
   {
-    id: 3,
-    name: "Shopping Cart",
-    liveUrl: "https://withmohitjoshi.github.io/shopping-cart/",
-    codeUrl: "https://github.com/withmohitjoshi/shopping-cart",
-    imageSrc: projectsImages.shoppingCart,
+    id: 1,
+    name: "Weather App",
+    liveUrl: "https://withmohitjoshi.github.io/weather-app/",
+    codeUrl: "https://github.com/withmohitjoshi/weather-app",
+    imageSrc: projectsImages.weatherApp,
   },
   {
-    id: 4,
+    id: 2,
     name: "Random Image Generator",
     liveUrl: "https://withmohitjoshi.github.io/random-image-generator/",
     codeUrl: "https://github.com/withmohitjoshi/random-image-generator",
     imageSrc: projectsImages.randomImageGenerator,
   },
-  // {
-  //   id: 5,
-  //   name: "Login Signup Page Ui",
-  //   liveUrl: "https://withmohitjoshi.github.io/login-signup-ui/",
-  //   codeUrl: "https://github.com/withmohitjoshi/login-signup-ui",
-  //   imageSrc: projectsImages.loginSignupUi,
-  // },
   {
-    id: 6,
+    id: 3,
     name: "Testimonial Grid Ui",
     liveUrl:
       "https://withmohitjoshi.github.io/FrontendMentor-Projects/testimonials-grid-section-main/",
@@ -46,21 +25,12 @@ export const projectsData = [
     imageSrc: projectsImages.testimonialGridComponentUi,
   },
   {
-    id: 7,
-    name: "QR Code Ui",
+    id: 4,
+    name: "Order Summary Ui",
     liveUrl:
-      "https://withmohitjoshi.github.io/FrontendMentor-Projects/qr-code-component-main/",
+      "https://withmohitjoshi.github.io/FrontendMentor-Projects/order-summary-component-main/",
     codeUrl:
-      "https://github.com/withmohitjoshi/FrontendMentor-Projects/tree/master/qr-code-component-main",
-    imageSrc: projectsImages.qrCodeComponentUi,
+      "https://github.com/withmohitjoshi/FrontendMentor-Projects/tree/master/order-summary-component-main",
+    imageSrc: projectsImages.orderSummaryComponentUi,
   },
-  // {
-  //   id: 8,
-  //   name: "Order Summary Ui",
-  //   liveUrl:
-  //     "https://withmohitjoshi.github.io/FrontendMentor-Projects/order-summary-component-main/",
-  //   codeUrl:
-  //     "https://github.com/withmohitjoshi/FrontendMentor-Projects/tree/master/order-summary-component-main",
-  //   imageSrc: projectsImages.orderSummaryComponentUi,
-  // },
 ];
